4e3750cbf52f1ad6de6b2c397270a2e346c60380
dthigpen <davidthigs@gmail.com>
2018-04-08 20:17:07 -0500

    initial geek code

--- client.c
+++ client.c
@@ -1,67 +1,44 @@
+// Client side C/C++ program to demonstrate Socket programming
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/types.h>
 #include <sys/socket.h>
+#include <stdlib.h>
 #include <netinet/in.h>
-#include <netdb.h>
-
-
-int main(int argc, char **argv){
-	int socketfd, portno;
-	struct sockaddr_in serv_addr;
-	struct hostent *server;
-	char buffer[256];
-	int TCP = 1;
-	int rw_code;
-	if (argc != 4)
-	{
-		printf("Error must have 3 arguments: server name, port number, and TCP/UDP} \n");
-		exit(0);
-	}
-	portno = atoi(argv[2]);
-	// initialize socket for TCP or UDP connection
-	if(strcmp(argv[3],"UDP")  || strcmp(argv[3], "udp")){
-		TCP = 0;
-		socketfd = socket(AF_INET,SOCK_DGRAM,0);
-	}
-	else if(strcmp(argv[3],"TCP")  || strcmp(argv[3], "tcp")){
-		TCP = 1;
-		socketfd = socket(AF_INET,SOCK_STREAM,0);
-	}
-	else{
-		printf("Error protocol must be either TCP or UDP \n");
-		exit(0);
-	}
-	if(socketfd < 0)
-	{
-		printf("Error opening socket");
-		exit(0);
-	}
-	
-	server = gethostbyname(argv[1]);
-	if (server == NULL) {
-        fprintf(stderr,"ERROR, no such host\n");
-        exit(0);
+#include <string.h>
+#define PORT 8080
+  
+int main(int argc, char const *argv[])
+{
+    struct sockaddr_in address;
+    int sock = 0, valread;
+    struct sockaddr_in serv_addr;
+    char *hello = "Hello from client";
+    char buffer[1024] = {0};
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        printf("\n Socket creation error \n");
+        return -1;
     }
-	memset((void *) &serv_addr,0, sizeof(serv_addr));
+  
+    memset(&serv_addr, '0', sizeof(serv_addr));
+  
     serv_addr.sin_family = AF_INET;
-    memmove( (char *)&serv_addr.sin_addr.s_addr, (char *)server->h_addr, server->h_length);
-    serv_addr.sin_port = htons(portno);
-    if (connect(socketfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) 
-        error("ERROR connecting");
-    printf("Please enter the message: ");
-    memset((void *)(buffer),0,256);
-    fgets(buffer,255,stdin);
-    rw_code = write(socketfd, buffer, strlen(buffer));
-    if (rw_code < 0) 
-         error("ERROR writing to socket");
-    memset((void *)(buffer),0,256);
-    rw_code = read(socketfd, buffer, 255);
-    if (rw_code < 0) 
-         error("ERROR reading from socket");
-    printf("%s\n", buffer);
-    close(socketfd);
+    serv_addr.sin_port = htons(PORT);
+      
+    // Convert IPv4 and IPv6 addresses from text to binary form
+    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) 
+    {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
+  
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
+    send(sock , hello , strlen(hello) , 0 );
+    printf("Hello message sent\n");
+    valread = read( sock , buffer, 1024);
+    printf("%s\n",buffer );
     return 0;
 }
--- server.c
+++ server.c
@@ -1,84 +1,58 @@
-#include <stdlib.h>
+// Server side C/C++ program to demonstrate Socket programming
 #include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h> 
 #include <sys/socket.h>
+#include <stdlib.h>
 #include <netinet/in.h>
-
-const int MAX_CONNECTIONS = 5;
-const int READ_BUFFER_SIZE = 256;
-enum Protocol{
-    UDP,
-    TCP
-}protocol;
-
-int main(int argc, char *argv[]){
-    int socketfd, new_socketfd, port_number,read_result;
-    socklen_t client_len;
-    char buffer[READ_BUFFER_SIZE];
-    struct sockaddr_in server_addr, client_addr;
-    
-
-
-    if(argc != 3){
-        printf("usage: %s [port 1024-65535] [tcp/udp]\n",argv[0]);
-        exit(0);
-    }else{
-        //set protocol type and create socket
-        if(strcasecmp(argv[1],"tcp")){
-            protocol = TCP;
-            socketfd = socket(AF_INET, SOCK_STREAM, 0);
-        }else if(strcasecmp(argv[1],"udp")){
-            protocol = UDP;
-            socketfd = socket(AF_INET, SOCK_DGRAM, 0);
-        }else{
-            printf("usage: %s [port 1024-65535] [tcp/udp]\n",argv[0]);
-            exit(0);
-        }
-        if(socketfd < 0){
-		printf("Error opening socket");
-	    }
-
-        memset((char *) &server_addr,sizeof(server_addr),0);
-        
-        //set port number
-        port_number = atoi(argv[1]);
-        if (port_number < 1024 || port_number > 65535){
-            printf("Invalid port number. Port range: 1024-65535\n");
-            exit(0);
-        }
-        server_addr.sin_family = AF_INET;
-        server_addr.sin_addr.s_addr = INADDR_ANY;
-        //swap the endianness of the port number for network order
-        server_addr.sin_port = htons(port_number);
-
-        if(bind(socketfd, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0){
-            printf("Error binding socket to IP address\n");
-            exit(0);
-        }
-
-        listen(socketfd,MAX_CONNECTIONS);
-        client_len = sizeof(client_addr);
-        
-        new_socketfd = accept(socketfd, (struct sockaddr *) &client_addr, client_len);
-        if(new_socketfd < 0){
-            printf("Error accepting connection\n");
-            exit(0);
-        }
-        
-        printf("[%s]: connection to %s on port %d",inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
-        send(new_socketfd,"Successful connection\n",13,0);
-    
-        memset((char *) &buffer, 0,sizeof(buffer));
-        read_result = read(new_socketfd, buffer, READ_BUFFER_SIZE - 1);
-        if(read_result < 0){
-            printf("Error reading message from client\n");
-        }
-        
-        close(new_socketfd);
-        close(socketfd);
-        
-        return 0;
+#include <string.h>
+#define PORT 8080
+int main(int argc, char const *argv[])
+{
+    int server_fd, new_socket, valread;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    char buffer[1024] = {0};
+    char *hello = "Hello from server";
+      
+    // Creating socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
     }
+      
+    // Forcefully attaching socket to the port 8080
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
+                                                  &opt, sizeof(opt)))
+    {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons( PORT );
+      
+    // Forcefully attaching socket to the port 8080
+    if (bind(server_fd, (struct sockaddr *)&address, 
+                                 sizeof(address))<0)
+    {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+    if (listen(server_fd, 3) < 0)
+    {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, 
+                       (socklen_t*)&addrlen))<0)
+    {
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+    valread = read( new_socket , buffer, 1024);
+    printf("%s\n",buffer );
+    send(new_socket , hello , strlen(hello) , 0 );
+    printf("Hello message sent\n");
+    return 0;
 }
