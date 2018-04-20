6848ef90357cabd5f4348a9ac44ddb4840bfd054
dthigpen <davidthigs@gmail.com>
2018-04-20 09:18:12 -0500

    add sending checksum and reporting back to server

--- client.c
+++ client.c
@@ -1,4 +1,4 @@
-// Client side C/C++ program to demonstrate Socket programming
+// Client side C/C++ program to demonstrate TCP Socket programming
 #include <stdio.h>
 #include <sys/socket.h>
 #include <stdlib.h>
@@ -6,60 +6,108 @@
 #include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
-#define PORT 8088
+#include <netdb.h>
+#include <openssl/md5.h>
+
 #define BUFFSIZE 1024
+#define MSGSIZE 16
 
-
-
+void generate_md5(FILE *file, char *result);
+void print_md5(char* result);
 
 int main(int argc, char const *argv[])
 {
+    struct addrinfo hints, *servinfo, *p;
     struct sockaddr_in address;
-    int sock = 0, valread;
+    int sock = 0, valread, rv;
     struct sockaddr_in serv_addr;
-    char *hello = "Connection Requested from client";
     char buffer[BUFFSIZE] = {0};
-    char work_buffer[BUFFSIZE*2];
-	const char *filename = argv[1];
+	const char *filename;
+    int PORT = 8088;
+    const char* server_ip;
+    char received_md5[MD5_DIGEST_LENGTH];
+	char calculated_md5[MD5_DIGEST_LENGTH];
+    char ack[]="ACK!";
+	char nack[] = "NACK";
     
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-    {
-        printf("\n Socket creation error \n");
-        return -1;
+    if(argc < 4){
+      printf("Error need exactly 3 arguments. Arg1: Server-IP Arg2: Port# Arg3: File Name\n");
+        exit(0);
+    }else{
+        server_ip = argv[1];
+        PORT = atoi(argv[2]);
+        filename =  argv[3];
     }
-  
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE; 
+
+    //argv[2] is the port# arg
+    if ((rv = getaddrinfo(server_ip, argv[2], &hints, &servinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+        return 1;
+    }
+
+    //connect to the first result from our getaddrinfo lookup on the specified ip
+    for(p = servinfo; p != NULL; p = p->ai_next) {
+        if ((sock = socket(p->ai_family, p->ai_socktype,
+                p->ai_protocol)) == -1) {
+            perror("client: socket");
+            continue;
+        }
+
+        break;
+    }
+
     memset(&serv_addr, '0', sizeof(serv_addr));
-  
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(PORT);
       
     // Convert IPv4 and IPv6 addresses from text to binary form
-    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) 
+    if(inet_pton(AF_INET, server_ip, &serv_addr.sin_addr)<=0) 
     {
-        printf("\nInvalid address/ Address not supported \n");
+        printf("\nInvalid address/Address not supported \n");
         return -1;
     }
-  
+
     if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
     {
         printf("\nConnection Failed \n");
         return -1;
     }
-    send(sock , hello , strlen(hello) , 0 );
     printf("Connection request sent\n");
 	
-	send(sock , filename , strlen(filename) , 0 );
-	printf("Sent request for filename\n");
-    valread = recv( sock , buffer, BUFFSIZE,0);
+  	// receive connection message
+  	valread = recv( sock , buffer, BUFFSIZE,0);
     printf("%s\n",buffer);
   	memset(&buffer,'\0',BUFFSIZE);
-    //RECEIVE SIZE INFO
+  
+  	//send the filename to the server
+	send(sock , filename , strlen(filename) , 0 );
+	printf("Filename request sent\n");
+    
+  	// Receive ACK or NACK message
+	valread = recv(sock,buffer,5,0);
+	printf("%s\n",buffer);
+  	if(buffer[0] == 'N')
+	{
+		printf("Error file does not exist in server.\n");
+		exit(1);
+	}
+	//Receive the checksum
+    memset(&buffer,'\0',BUFFSIZE);
+    valread = recv(sock,buffer,BUFFSIZE,0);
+    memcpy(received_md5,buffer,MD5_DIGEST_LENGTH);
+
+  
+  	//RECEIVE SIZE INFO
     //next message should be file size
-    int read_code = recv(sock, buffer, BUFFSIZE,0);
+    int read_code = recv(sock, buffer, MSGSIZE,0);
     printf("%s\n",buffer);
     int size = atoi(buffer);
     //RECEIVE SIZE INFO
-    
     printf("Received size of file: %d bytes\n", size);
   	int bytes_received = 0;
   	FILE *jpeg;
@@ -67,9 +115,9 @@ int main(int argc, char const *argv[])
     int fail_count = 0;
     int write_fail_count = 0;
     int packets_received = 0;
-    int wb_offset = 0;
-  	jpeg = fopen("out_file.jpg","wb");
-    memset(&work_buffer,'\0',BUFFSIZE*2);	
+    
+  	jpeg = fopen("out_file.jpg","rb+");
+  	
   	while(bytes_received < size){
         
         memset(&buffer,'\0',BUFFSIZE);
@@ -78,20 +126,11 @@ int main(int argc, char const *argv[])
       	if(valread < 0){ //an error has occured
             printf("Error recieving packet\n");
             fail_count++;
-            continue;
+            break;
         }else if(valread == 0){ //the connection has been closed
             printf("Server has closed the connection\n");
             break;
         }else{
-
-            wb_offset += valread;
-            if(wb_offset > BUFFSIZE){
-                //then a full packet has been received
-                //TODO write first BUFFSIZE of work_buffer to file
-                //TODO shift over work_buffer by BUFF_SIZE
-            }else{
-                //still a partial packet OR last packet
-            }
             packets_received++;
             int write_error = fwrite(buffer,sizeof(char),valread, jpeg);
             if(write_error <= 0){
@@ -102,11 +141,44 @@ int main(int argc, char const *argv[])
             // printf("%s\n",buffer);
         }
     }
-    
-    memset(&buffer,'\0',BUFFSIZE);
+    fclose(jpeg);
+    //check the received md5 with the calculated one
+    jpeg = fopen("out_file.jpg","rb");
+    generate_md5(jpeg, calculated_md5);
+    printf("Calculated MD5:\n");
+    print_md5(calculated_md5);
+    printf("Received MD5:\n");
+    print_md5(received_md5);
+
+    if(strncmp(calculated_md5, received_md5, MD5_DIGEST_LENGTH) == 0){
+        //send match
+        send(sock,ack,strlen(ack),0);
+        printf("Checksums match\n");
+    }else{
+        //send mismatch
+        send(sock,nack,strlen(nack),0);
+        printf("Checksum mismatch\n");
+    }
     printf("Write fails: %d\n", write_fail_count);
     printf("File received\n");
   	fclose(jpeg);
     shutdown(sock,2);
     return 0;
+}
+
+void generate_md5(FILE *file, char* result){
+    MD5_CTX mdContext;
+    int i, bytes;
+    unsigned char data[1024];
+    MD5_Init (&mdContext);
+    while ((bytes = fread (data, 1, 1024, file)) != 0)
+        MD5_Update (&mdContext, data, bytes);
+    MD5_Final (result,&mdContext);
+    rewind(file);
+}
+
+void print_md5(char* result){
+	int i = 0;
+    for(i = 0; i < MD5_DIGEST_LENGTH; i++) printf("%02x", result[i]);
+    printf("\n");
 }
--- server.c
+++ server.c
@@ -1,36 +1,83 @@
-// Server side C/C++ program to demonstrate Socket programming
+// Server side C/C++ program to demonstrate TCP Socket programming 
 #include <stdio.h>
 #include <sys/socket.h>
+#include <netdb.h>
 #include <stdlib.h>
 #include <netinet/in.h>
 #include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
-#define PORT 8088
+#include <openssl/md5.h>
+
 #define BUFFSIZE 1024
+#define ADDR_STR_LEN 50
 
-
-int sendall(int s,char* buffer, int*len);
+int sendall(int s,char *buffer, int*len);
+void generate_md5(FILE *file, char *result);
+void print_md5(char* result);
 
 int main(int argc, char const *argv[])
 {
-    int server_fd, new_socket, valread;
+    struct addrinfo hints, *servinfo, *p;
+    int server_fd, new_socket, valread, rv;
     struct sockaddr_in address;
     int opt = 1;
     int addrlen = sizeof(address);
     char buffer[BUFFSIZE];
     char *connection = "Connection granted to client\n";
     FILE* jpeg;
-  
-    // Creating socket file descriptor
-    //TCP
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
-    {
-        perror("socket failed");
-        exit(EXIT_FAILURE);
+  	int PORT;
+    char* file_md5_buffer;
+    char md5_result[MD5_DIGEST_LENGTH];
+  	char ack[]="ACK!";
+	char nack[] = "NACK";
+  	
+  	if(argc < 2){
+      printf("Error need exactly 1 argument. Arg1: Port Number\n");
+      exit(0);
     }
-      
-    // Forcefully attaching socket to the port 8080
+  	
+  	PORT = atoi(argv[1]);
+  
+
+    // Creating socket file descriptor
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_INET; // set to AF_INET to force IPv4
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE ; // use my IP, host name
+
+    
+
+    if ((rv = getaddrinfo(NULL, argv[1], &hints, &servinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+        return 1;
+    }
+
+    // loop through all the results and bind to the first we can
+    for(p = servinfo; p != NULL; p = p->ai_next) {
+        if ((server_fd = socket(p->ai_family, p->ai_socktype,
+                p->ai_protocol)) == -1) {
+            perror("listener: socket");
+            continue;
+        }
+
+        if (bind(server_fd, p->ai_addr, p->ai_addrlen) == -1) {
+            close(server_fd);
+            perror("listener: bind");
+            continue;
+        }
+        
+        break;
+    }
+
+    if (p == NULL) {
+        fprintf(stderr, "listener: failed to bind socket\n");
+        return 2;
+    }
+
+    freeaddrinfo(servinfo);
+    //SETUP FINISHED
+    // Forcefully attaching socket to the port 
     if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
                                                   &opt, sizeof(opt)))
     {
@@ -40,14 +87,8 @@ int main(int argc, char const *argv[])
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons( PORT );
-      
-    // Forcefully attaching socket to the port 8080
-    if (bind(server_fd, (struct sockaddr *)&address, 
-                                 sizeof(address))<0)
-    {
-        perror("bind failed");
-        exit(EXIT_FAILURE);
-    }
+
+    printf("Waiting for a client to connect\n");
     if (listen(server_fd, 3) < 0)
     {
         perror("listen");
@@ -59,39 +100,48 @@ int main(int argc, char const *argv[])
         perror("accept");
         exit(EXIT_FAILURE);
     }
+  	// send connection message
     memset(buffer, '\0',BUFFSIZE);
-    valread = read( new_socket , buffer, BUFFSIZE);
-    printf("%s\n",buffer );
     send(new_socket , connection , strlen(connection) , 0 );
-    
   	memset(buffer, '\0',BUFFSIZE);
+  
   	// read in the name of the file to send
   	valread = read(new_socket,buffer,BUFFSIZE);
-  	jpeg = fopen(buffer,"rb");
+  	jpeg = fopen(buffer,"rb+");
+  	
+  	if(jpeg == NULL){
+		// if file does not exist send NACK
+		send(new_socket,nack,strlen(nack),0);
+	}
   	if(jpeg != NULL){
-        //send file
+      	// if file exists send ACK 
+		send(new_socket,ack,strlen(ack),0);
+      	
+      	// if file exists do md5 checksum and send it
+		generate_md5(jpeg, md5_result);
+        memset(buffer, '\0',BUFFSIZE);
+        memcpy(buffer,md5_result,BUFFSIZE);
+        send(new_socket,buffer,BUFFSIZE,0);
+ 
       	int size = 0;
       	int num_bytes_sent = 0;
       	int result = 0;
       	int bytes_sent = 0;
       	int max_bytes_to_send = BUFFSIZE;
-      	int count = 0;
-        int sent_count = 0;
       	fseek(jpeg, 0, SEEK_END);
       	size = ftell(jpeg);
       	rewind(jpeg);
         
-        //SEND SIZE INFO
         //send the size of the file to the client first
-        char* size_str[16];
+        char size_str[16];
         sprintf(size_str,"%d",size);
       	send(new_socket,size_str,sizeof(size_str),0);
       	printf("Sending file size: %s\n",size_str);
-        //SEND SIZE INFO
         
-        printf("Calculated File size: %d", size);
+        printf("Calculated File size: %d \n", size);
         
       	while(num_bytes_sent < size){
+
             memset(buffer, '\0',BUFFSIZE);
             int bytes_left = size - ftell(jpeg);
             int bytes_read = fread(&buffer,sizeof(char),BUFFSIZE,jpeg);
@@ -101,37 +151,35 @@ int main(int argc, char const *argv[])
             }
             if(bytes_read < 0){
                 printf("Error reading file\n");
-                //TODO file read error
                 break;
             }
             //the last section of the file may be smaller than the buffer size
             int send_size = bytes_left < BUFFSIZE ? bytes_read : BUFFSIZE;
             //send all of the data in the buffer so far
-            if(send_size != BUFFSIZE){
-                printf("new send size: %d\n",send_size);
-            }
+
             result = sendall(new_socket,buffer,&send_size);
             if(result == -1){
                 printf("Failed to send buffered data\n");
-                //TODO failed to send error
                 break;
             }else{
-                num_bytes_sent += BUFFSIZE;
+                num_bytes_sent += send_size;
                 printf("bytes sent: %d / %d\n",num_bytes_sent,size);
             }
         }
-        printf("after loop bytes sent: %d / %d\n",num_bytes_sent,size);
-        printf("Fail count: %d\n",count);
+        printf("bytes sent: %d / %d\n",num_bytes_sent,size);
         printf("File sent\n");
-        
+        //wait for checksum ack or nack
+        memset(buffer, '\0',BUFFSIZE);
+        valread = recv(new_socket,buffer,5,0);
+        if(buffer[0] == 'N'){
+            printf("Client reports a checksum mismatch. Send corrupted\n");
+        }else{
+            printf("Client reports checksums match. Send successful\n");
+        }
 
-    }else{
-      	//send neg ack
-      	printf("File does not exist\n");
     }
-
-    shutdown(new_socket, 1);
-  	printf("Any more requests?\n");
+	
+    shutdown(new_socket, 2);
   	fclose(jpeg);  
   	return 0;
 }
@@ -141,9 +189,9 @@ int sendall(int s, char* buffer, int *len)
     int total = 0;        // how many bytes we've sent
     int bytesleft = *len; // how many we have left to send
     int n;
-
+	
     while(total < *len) {
-        n = send(s, buffer, bytesleft, 0);
+        n = send(s, buffer + total , bytesleft, 0);
         if (n == -1) { break; }
         total += n;
         bytesleft -= n;
@@ -153,3 +201,20 @@ int sendall(int s, char* buffer, int *len)
 
     return n==-1?-1:0; // return -1 on failure, 0 on success
 } 
+
+void generate_md5(FILE *file, char* result){
+    MD5_CTX mdContext;
+    int i, bytes;
+    unsigned char data[1024];
+    MD5_Init (&mdContext);
+    while ((bytes = fread (data, 1, 1024, file)) != 0)
+        MD5_Update (&mdContext, data, bytes);
+    MD5_Final (result,&mdContext);
+    rewind(file);
+}
+
+void print_md5(char* result){
+	int i = 0;
+    for(i = 0; i < MD5_DIGEST_LENGTH; i++) printf("%02x", result[i]);
+    printf("\n");
+}
